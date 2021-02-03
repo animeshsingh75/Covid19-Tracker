386f09dbf73398328495e7c69eed29fb21c08fcb
Animesh Singh <animeshsingh75@gmail.com>
2021-02-03 17:10:29 +0530

    Added container

--- app/src/main/java/com/example/covid19_tracker/MainActivity.kt
+++ app/src/main/java/com/example/covid19_tracker/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.example.covid19_tracker
 
-import java.text.SimpleDateFormat
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.example.covid19_tracker.databinding.ActivityMainBinding
 import com.google.gson.Gson
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.text.DecimalFormat
+import java.text.NumberFormat
+import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.TimeUnit
 
@@ -26,7 +29,7 @@ class MainActivity : AppCompatActivity() {
             val response = withContext(Dispatchers.IO) { Client.api.execute() }
             if (response.isSuccessful) {
                 val data = Gson().fromJson(response.body?.string(), Response::class.java)
-                launch(Dispatchers.Main){
+                launch(Dispatchers.Main) {
                     bindCombinedData(data.statewise?.get(0))
                 }
             }
@@ -35,26 +38,67 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun bindCombinedData(data: StatewiseItem) {
-        val lastUpdatedTime=data.lastupdatedtime
-        val simpleDateFormat=SimpleDateFormat("dd/MM/yyyy HH:mm:ss")
-        binding.lastUpdatedTv.text="Last Updated\n ${getTimeAgo(simpleDateFormat.parse(lastUpdatedTime))}"
+        val lastUpdatedTime = data.lastupdatedtime
+        val simpleDateFormat = SimpleDateFormat("dd/MM/yyyy HH:mm:ss")
+        binding.apply {
+            lastUpdatedTv.text =
+                "Last Updated\n ${getTimeAgo(simpleDateFormat.parse(lastUpdatedTime))}"
+            val confirmed = getFormatedAmount(data.confirmed!!.toInt())
+            val active = getFormatedAmount(data.active!!.toInt())
+            val deaths = getFormatedAmount(data.deaths!!.toInt())
+            val recovered = getFormatedAmount(data.recovered!!.toInt())
+            confirmedTv.text = confirmed
+            activeTv.text = active
+            deceasedTv.text = deaths
+            recoveredTv.text = recovered
+        }
+
     }
-    fun getTimeAgo(past:Date):String{
-        val now=Date()
-        val seconds=TimeUnit.MILLISECONDS.toSeconds((now.time)-past.time)
-        val minutes=TimeUnit.MILLISECONDS.toMinutes((now.time-past.time))
-        val hours=TimeUnit.MILLISECONDS.toHours(now.time-past.time)
-        return when{
-            seconds<60->{
+
+    private fun getFormatedAmount(amount: Int): String? {
+        if (amount > 10000000) {
+            var cr = amount / 10000000
+            val remainder = amount-cr*10000000
+            val lakh = remainder / 100000
+            if(lakh<10){
+                return "$cr.0$lakh Cr"
+            }
+            else{
+                return "$cr.$lakh Cr"
+            }
+        }
+        else if(amount>100000){
+            var lakh = amount / 100000
+            val remainder = amount-lakh*100000
+            val thousand = remainder / 1000
+            if(thousand<10){
+                return "$lakh.0$thousand L"
+            }
+            else{
+                return "$lakh.$thousand L"
+            }
+        }
+        val formatter = DecimalFormat("##,###")
+        return formatter.format(amount)
+
+    }
+
+    fun getTimeAgo(past: Date): String {
+        val now = Date()
+        val seconds = TimeUnit.MILLISECONDS.toSeconds((now.time) - past.time)
+        val minutes = TimeUnit.MILLISECONDS.toMinutes((now.time - past.time))
+        val hours = TimeUnit.MILLISECONDS.toHours(now.time - past.time)
+        return when {
+            seconds < 60 -> {
                 "Few seconds ago"
             }
-            minutes<60->{
+            minutes < 60 -> {
                 "$minutes minutes ago"
             }
-            hours<24->{
-                "$hours hour ${minutes%60} min ago"
+            hours < 24 -> {
+                "$hours hour ${minutes % 60} min ago"
             }
-            else->{
+            else -> {
                 SimpleDateFormat("dd/MM/yy, hh:mm a").format(past).toString()
             }
         }
